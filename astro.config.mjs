197d9396a8c3f9aa50e4641e65cb55b5e51e63c7
Ian O'Gorman <ogorman89@gmail.com>
2023-12-13 16:30:00 -0800

    add config.ts and update refs

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,4 +16,5 @@ export default defineConfig({
     }),
     robotsTxt(),
   ],
+  experimental: { assets: true },
 });
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = `Ian's Personal Site`;
+export const SITE_DESCRIPTION = 'Welcome to my blog/portfolio!';
